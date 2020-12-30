981cc6c4ad9c58d5165f3e5c31ea85ff9d932775
mlodyFrontendowiec <pawel.kruszelnicki@onet.pl>
2020-12-30 17:06:24 +0100

    Create class Game

--- app/Game.js
+++ app/Game.js
@@ -1,1 +1,16 @@
-console.log("ok");
+export class Game {
+  constructor({
+    lettersWrapper,
+    categoryWrapper,
+    wordWrapper,
+    outputWrapper,
+  } = obj) {
+    this.lettersWrapper = lettersWrapper;
+    this.categoryWrapper = categoryWrapper;
+    this.wordWrapper = wordWrapper;
+    this.outputWrapper = outputWrapper;
+  }
+  start() {
+    console.log("Lets play the game");
+  }
+}
---
+++ app/index.js
@@ -0,0 +1,8 @@
+import { Game } from "./Game.js";
+const game = new Game({
+  lettersWrapper: document.getElementById("letters"),
+  categoryWrapper: document.getElementById("category"),
+  wordWrapper: document.getElementById("word"),
+  outputWrapper: document.getElementById("output"),
+});
+game.start();
