7230dbb285dad3dbc4c8fcc017679395025b68c6
mlodyFrontendowiec <pawel.kruszelnicki@onet.pl>
2020-12-30 18:46:18 +0100

    Add images and add steps

--- app/Game.js
+++ app/Game.js
@@ -1,5 +1,7 @@
 import { Quote } from "./Quote.js";
 export class Game {
+  currentStep = 0;
+  lastStep = 8;
   quotes = [
     {
       text: "pan tadeusz",
@@ -35,25 +37,37 @@ export class Game {
     this.categoryWrapper.innerHTML = category;
     this.quote = new Quote(text);
   }
-  guess(letter) {
-    console.log(letter);
+  guess(letter, e) {
+    e.target.disabled = true;
+    if (this.quote.guess(letter)) {
+      this.drawQuote();
+    } else {
+      this.currentStep++;
+      document.getElementsByClassName("step")[
+        this.currentStep
+      ].style.opacity = 1;
+    }
   }
   drawLetters() {
     for (let i = 0; i < 26; i++) {
       const label = (i + 10).toString(36);
       const button = document.createElement("button");
       button.innerHTML = label;
-      button.addEventListener("click", () => {
-        this.guess(label);
+      button.addEventListener("click", (e) => {
+        this.guess(label, e);
       });
       this.lettersWrapper.appendChild(button);
     }
   }
 
-  start() {
-    this.drawLetters();
-
+  drawQuote() {
     const content = this.quote.getContent();
     this.wordWrapper.innerHTML = content;
+  }
+
+  start() {
+    document.getElementsByClassName("step")[this.currentStep].style.opacity = 1;
+    this.drawLetters();
+    this.drawQuote();
   }
 }
